d3069c0768c7ef7cbc21937197781056c2297309
Furkan Ozbek <furkanozbek1995@gmail.com>
2019-10-10 19:55:41 +0300

    card component eklendi
    Card componenti güncellenmeye başlandı

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-card -light">
+    <h2 class="m-card__header">{{movie.Title}}</h2>
+
+    <img class="m-card__image" :src="movie.Poster" alt="movie poster" />
+
+    <p class="m-card__paragraph">{{movie.Plot}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["movie"]
+};
+</script>
+
+<style>
+</style>
